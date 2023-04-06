4f09eab83068825d6fa4c4be54f5913e83435bac
shahriaarrr <shahriaarrr@gmail.com>
2023-04-07 03:19:32 +0430

    add home page view

--- config/config/settings.py
+++ config/config/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
+        'DIRS': []
         ,
         'APP_DIRS': True,
         'OPTIONS': {
--- config/core/views.py
+++ config/core/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def corepage(request):
+    return render(request, 'core/home.html')
