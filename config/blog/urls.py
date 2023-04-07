d64e4fc9956dedb3a91bca548a48f01222c110ec
shahriaarrr <shahriaarrr@gmail.com>
2023-04-07 11:07:22 +0430

    edit name of the index url page

--- config/blog/urls.py
+++ config/blog/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', Index.as_view(), name="corepage"),
+    path('', Index.as_view(), name="index"),
     path('about/', About.as_view(), name='about'),
 ]
