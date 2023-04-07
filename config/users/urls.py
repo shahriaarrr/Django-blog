97271e11bded2c9c8239c6ae9f5d1a659a847c10
shahriaarrr <shahriaarrr@gmail.com>
2023-04-07 19:48:20 +0430

    add login form and logout views

--- config/users/urls.py
+++ config/users/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from .views import *
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name="login"),
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name="logout"),
     path('register/', RegisterView.as_view(), name="register"),
 ]
