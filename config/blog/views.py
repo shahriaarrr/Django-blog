d46885f949ca65b411232a687ded046076550838
shahriaarrr <shahriaarrr@gmail.com>
2023-04-08 21:05:20 +0430

    change home view and template to listview

--- config/blog/views.py
+++ config/blog/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from django.views import View
+from django.views.generic import ListView
+
+from .models import Article
 # Create your views here.
 
-class Index(View):
-    def get(self, request):
-        return render(request, 'blog/home.html')
+class Index(ListView):
+    model = Article
+    queryset = Article.objects.all().order_by("-date")
+    template_name = 'blog/home.html'
 
 
 class About(View):
