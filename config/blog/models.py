eadaddc7aae7db16769ca2011593561d71efaacf
shahriaarrr <shahriaarrr@gmail.com>
2023-04-08 20:48:07 +0430

    add Article model

--- config/blog/models.py
+++ config/blog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
-
+from tinymce.models import HTMLField
+from django.contrib.auth.models import User
 # Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=255)
+    content = HTMLField()
+    date = models.DateField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    featured = models.BooleanField(default=False)
+    likes = models.ManyToManyField(User, related_name="likes")
+
+    def __str__(self):
+        return self.title
